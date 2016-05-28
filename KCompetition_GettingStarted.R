ad5e7d7dc710c01277497f51170dfd4ad5f606ee
Guillaume Bersac <bersac_1@hotmail.fr>
2016-05-28 15:47:50 +0200

    test archtecture ok

--- KCompetition_GettingStarted.R
+++ KCompetition_GettingStarted.R
@@ -1,25 +1,51 @@
-# KAGGLE COMPETITION - GETTING STARTED
+finalOutput = FALSE
 
-# This script file is intended to help you get started on the Kaggle platform, and to show you how to make a submission to the competition.
+################################################################################
+# Feature engineering                                                          #
+################################################################################
 
+# Read data from files
+data = read.csv("train2016.csv")
+evaluation = read.csv("test2016.csv")
 
-# Let's start by reading the data into R
-# Make sure you have downloaded these files from the Kaggle website, and have navigated to the directory where you saved the files on your computer
+### Remove all NA values
+data[is.na(data)] <- 1980
 
-train = read.csv("train2016.csv")
-test = read.csv("test2016.csv")
+### Creating train and test set
+if (finalOutput) {
+	train = data
+	test = evaluation
+} else {
+	library(caTools)
+	set.seed(42)
+	spl = sample.split(data$Party, SplitRatio = 0.7)
+	train = subset(data, spl==TRUE)
+	test = subset(data, spl==FALSE)
+}
+
+################################################################################
+# Creation model                                                               #
+################################################################################
 
 # We will just create a simple logistic regression model, to predict Party using all other variables in the dataset, except for the user ID:
-SimpleMod = glm(Party ~ . -USER_ID, data=train, family=binomial)
+model = glm(Party ~ . - USER_ID, data=train, family=binomial)
 
-# And then make predictions on the test set:
-PredTest = predict(SimpleMod, newdata=test, type="response")
+# And then make predictions on the evaluation set:
+predictions = predict(model, newdata=test)
 
+###############################################################################
+# Output                                                                       #
+################################################################################
 threshold = 0.5
 
-PredTestLabels = as.factor(ifelse(PredTest<threshold, "Democrat", "Republican"))
+if (finalOutput) {
+	PredTestLabels = as.factor(ifelse(predictions<threshold, "Democrat", "Republican"))
 
-# Let's prepare a submission file for Kaggle (for more about this, see the "Evaluation" page on the competition site):
-MySubmission = data.frame(USER_ID = test$USER_ID, PREDICTION = PredTestLabels)
-colnames(MySubmission) <- c("USER_ID","Predictions")
-write.csv(MySubmission, "SubmissionSimpleLog.csv", row.names=FALSE, quote=FALSE)
+	# Let's prepare a submission file for Kaggle (for more about this, see the "Evaluation" page on the competition site):
+	MySubmission = data.frame(USER_ID = evaluation$USER_ID, PREDICTION = PredTestLabels)
+	colnames(MySubmission) <- c("USER_ID","Predictions")
+	write.csv(MySubmission, "SubmissionSimpleLog.csv", row.names=FALSE, quote=FALSE)
+} else {
+	confusionMatrix = table(test$Party, predictions > threshold)
+	cat("accuracy", sum(diag(confusionMatrix))/sum(confusionMatrix), "\n")
+}
